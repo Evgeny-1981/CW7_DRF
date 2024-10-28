c1925ebd6117febef739342a6e4cb815d52bca4e
Evgeny Egorov <EAEgorov42@gmail.com>
2024-10-28 23:43:36 +0500

    .

--- habits/tasks.py
+++ habits/tasks.py
@@ -10,15 +10,13 @@ from habits.services import send_telegram_message
 def telegram_sender():
     """Отправка сообщения из бота в Телеграм с напоминанием о привычке"""
     habits = Habit.objects.all().order_by("time")
-    print(habits)
     current_time = datetime.now().time()
-    print(current_time)
     for habit in habits:
-        print(habit)
         if habit.owner.telegram_id and habit.time > current_time:
             print("Все есть")
             tg_chat_id = habit.owner.telegram_id
+            print(tg_chat_id)
             message = habit.__str__()
             send_telegram_message(tg_chat_id, message)
         else:
-            print("Нет привычек")
+            print("Нет привычеккккк")

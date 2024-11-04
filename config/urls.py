654aac14b755348e949b42b0532c8823c5b52acd
Evgeny Egorov <EAEgorov42@gmail.com>
2024-11-05 00:13:36 +0500

    .

--- config/urls.py
+++ config/urls.py
@@ -12,9 +12,10 @@ schema_view = get_schema_view(
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@snippets.local"),
         license=openapi.License(name="BSD License"),
-        public=True,
-        permission_classes=(permissions.AllowAny,),
-    ),)
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
